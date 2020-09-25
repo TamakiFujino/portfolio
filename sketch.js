586e241f9b5d379f337bbc7724418468369f7a38
TamakiFujino <42616711+TamakiFujino@users.noreply.github.com>
2020-09-25 01:05:41 -0700

    a

--- sketch.js
+++ sketch.js
@@ -1,18 +1,3 @@
-// // Daniel Shiffman
-// // http://codingtra.in
-// // http://patreon.com/codingtrain
-// // Code for: https://youtu.be/KkyIDI6rQJI
-//
-// // Purple Rain
-// // (138, 43, 226)
-// // (230, 230, 250) // background
-//
-// // var img;
-//
-// function preload() {
-//   // you will need to change this to the file path to your image
-//   img = loadImage('images/rain2.png');
-// }
 //
 // var drops = [];
 //
@@ -43,21 +28,69 @@
 // (138, 43, 226)
 // (230, 230, 250) // background
 
-var drops = [];
+// var drops = [];
+//
+// function setup() {
+//  createCanvas(windowWidth, windowHeight);
+//  for (var i = 0; i < 500; i++) {
+//    drops[i] = new Drop();
+//  }
+//  var cnv = createCanvas(windowWidth, windowHeight);
+// cnv.style('display', 'block');
+// }
+//
+// function draw() {
+//  background(rgb(0,0,0));
+//  for (var i = 0; i < drops.length; i++) {
+//    drops[i].fall();
+//    drops[i].show();
+//  }
+// }
 
-function setup() {
- createCanvas(windowWidth, windowHeight);
- for (var i = 0; i < 500; i++) {
-   drops[i] = new Drop();
- }
- var cnv = createCanvas(windowWidth, windowHeight);
-cnv.style('display', 'block');
+class Drop{
+
+    constructor(){
+        this.x = random(0,width);
+        this.y = random(-200,-100);
+        this.z = random(0,20);
+        this.yspeed = map(this.z,0,10,2,10);
+    }
+    fall(){
+        this.y = this.y + this.yspeed;
+        var g = map(this.z,0,20,0,0.2);
+        this.yspeed = this.yspeed + g;
+        if(this.y > height){
+            this.y = random(-200,-100);
+            this.yspeed = random(2,10);
+        }
+    }
+    show(){
+        var l = map(this.z,0,20,10,20);
+        var thickness = map(this.z,0,20,1,1);
+        strokeWeight(thickness);
+        stroke(255,255,255);
+        line(this.x,this.y,this.x,this.y+l);
+    }
 }
 
-function draw() {
- background(255, 255, 255);
- for (var i = 0; i < drops.length; i++) {
-   drops[i].fall();
-   drops[i].show();
- }
+
+var drops=[];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    for(var i=0;i<480;i++)
+    {
+        drops[i] = new Drop();
+    }
+
+}
+
+function draw()
+{
+    background(173,180,168);
+    for(var i=0;i<drops.length;i++)
+    {
+        drops[i].fall();
+        drops[i].show();
+    }
 }
